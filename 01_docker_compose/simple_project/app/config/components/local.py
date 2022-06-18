77e45e727a0081ff9658ee736e8bb614fe59c238
xh4vm <xoklhyip@yandex.ru>
2022-06-18 16:43:43 +0500

    Nginx

--- 01_docker_compose/simple_project/app/config/components/local.py
+++ 01_docker_compose/simple_project/app/config/components/local.py
@@ -1,5 +1,11 @@
+import os 
+
+
 INTERNAL_IPS = [
     '127.0.0.1',
 ]
 
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, '..', 'static', 'django')
